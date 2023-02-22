f998334d61c4893146758d90ecc2657a7a57b9eb
RafaVera <jr.vera.ma@gmail.com>
2023-02-22 21:13:34 +0100

    Ejercicio 9

---
+++ src/Ejercicio_9_Factorial_recursividad.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicio_9_Factorial_recursividad {
+
+    public static void main (String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Introduce un número: ");
+        int numero = teclado.nextInt();
+        teclado.close();
+        System.out.println("\n El factorial de " + numero + " es:" + factorial(numero));
+
+
+    }
+
+    public static int factorial (int numero) {
+        if (numero == 0)
+            return 1;
+        else
+            return numero * factorial(numero-1);
+    }
+}
