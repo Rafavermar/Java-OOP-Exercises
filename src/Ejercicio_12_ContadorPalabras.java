0a85612e4ec0ff40f14fef81a53420cce83a8ace
RafaVera <rafael.vera@babelgroup.com>
2023-02-23 22:32:09 +0100

    Ejercicio_12_ContadorPalabras

---
+++ src/Ejercicio_12_ContadorPalabras.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ejercicio_12_ContadorPalabras {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Introduce una cadena de texto: ");
+        String cadenaUsuario = teclado.nextLine();
+        System.out.println("\nLa cadena tiene " +cuentaPalabras(cadenaUsuario) + " palabras");
+
+
+    }
+    public static int cuentaPalabras(String cadena) {
+        String[] palabras = cadena.trim().split(" +");
+        return palabras.length;
+    }
+}
