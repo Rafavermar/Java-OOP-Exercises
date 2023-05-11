7cbfdb4bce3d5411ebbbe519964b8fe2b3887f44
RafaVera <rafael.vera@babelgroup.com>
2023-05-11 11:16:19 +0200

    Encapsulamiento

---
+++ src/Ejercicio_26_AbstraccionEncapsulamiento/Book.java
@@ -0,0 +1,34 @@
+package Ejercicio_26_AbstraccionEncapsulamiento;
+
+public class Book {
+    private String title;
+    private String author;
+
+
+
+    private boolean available;
+
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
